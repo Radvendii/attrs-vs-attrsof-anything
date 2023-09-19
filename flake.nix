e896c2bf31dc82cb2351dc8a9eaffedcb1dbba34
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-19 11:11:11 -0400

    better naming

--- flake.nix
+++ flake.nix
@@ -5,31 +5,26 @@
 
   outputs = { self, nixpkgs }: {
 
-    nixosConfigurations.uh-oh = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        ({lib, ... }: {
-          options.foo = lib.mkOption {
+      modules = [ ({lib, ... }: {
+
+        options = {
+          attrs = lib.mkOption {
             type = lib.types.attrs;
           };
-          config.foo = { a = { b = lib.mkDefault 1; }; };
-        })
-      ];
-    };
-
-    nixosConfigurations.no-problem = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({lib, ... }: {
-          options.foo = lib.mkOption {
+          attrsOf = lib.mkOption {
             type = lib.types.attrsOf lib.types.anything;
           };
-          config.foo = { a = { b = lib.mkDefault 1; }; };
-        })
-      ];
-    };
+        };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        config = {
+          attrs.a.b = lib.mkDefault 1;
+          attrsOf.a.b = lib.mkDefault 1;
+        };
+
+      })];
+    };
 
   };
 }
